fb0a2c28d2729184462642130e96936fa0ca8217
Elizabeth Sobolik <elizabethboylesobolik@dyn-160-39-148-118.dyn.columbia.edu>
2018-04-27 18:26:39 -0400

    added new conversion table

--- plot_script.R
+++ plot_script.R
@@ -1,15 +1,29 @@
+# A script that loads all the sleuth objects and outputs a volcano plot
+# of the Wald beta-parameters and a csv file of the gene expression results
+
 library("sleuth")
 library("biomaRt")
+
+# First, gather all the filepaths/names of the sleuth objects to open
 setwd("/Users/elizabethboylesobolik/Desktop/sleuth_objects/")
 p <- "/Users/elizabethboylesobolik/Desktop"
 r <- "sleuth_objects"
 sleuth_objs <- (dir(file.path(p,r)))
-print(sleuth_objs)
+
+# Now open, plot each one!
 for (s in sleuth_objs){
+  # load the sleuth object
   so <- sleuth_load(s)
+  
+  # get a summary of the models computed
   models <- models(so)
+  
+  # get the pointer to the full model
   full_model <- models[1]
+  # then get first information in the full model (beta values)
   fm <- full_model[[1]]
+  
+  # then get the value of the first beta value
   k <- fm[[1]]
   list_info <- k[[1]]
   condition_name <- list_info$b1
